a741c5fbae6bf241f85de6787b34d7b93cc055a0
Milana46 <bogatyrevamilana462004@gmail.com>
2024-11-27 18:19:33 +0300

    OOP with  function-constructor

--- doptaskAboutOOP/task1.js
+++ doptaskAboutOOP/task1.js
@@ -11,82 +11,86 @@
 //Object.assign(User.prototype,{qmethode1(),methode2()})
 
 
-class Pet {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+function Pet(name, age) {
+    this.name = name;
+    this.age = age;
+  
+    this.describe = function () {
+      return `Имя: ${this.name}, возраст: ${this.age}`;
+    };
+  
+    this.isOld = function () {
+      return this.age > 10;
+    };
+  }
+  
+  
+  Pet.compareAges = function (pet1, pet2) {
+    if (pet1.age > pet2.age) {
+      return "Первый питомец старше второго";
+    } else if (pet2.age > pet1.age) {
+      return "Второй питомец старше первого";
+    } else {
+      return "Оба питомца одинакового возраста";
     }
+  };
+  
+  
+  function Dog(name, age, breed) {
+    Pet.call(this, name, age); 
+    this.breed = breed;
+  
+    this.describe = function () {
+      return `Имя: ${this.name}, возраст: ${this.age}, порода: ${this.breed}`;
+    };
+  
+    this.bark = function () {
+      console.log(`${this.name} лает`);
+    };
+  
+    this.fetch = function (item) {
+      console.log(`${this.name} приносит ${item}`);
+    };
+  }
 
-    describe() {
-        return `Имя: ${this.name}, возраст: ${this.age}`;
-    }
-
-    isOld() {
-        return this.age > 10;
-    }
-
-    static compareAges(pet1, pet2) {
-        if (pet1.age > pet2.age) {
-            return "Первая собака старше второй";
-        } else if (pet2.age > pet1.age) {
-            return "Вторая собака старше первой";
-        } else {
-            return "Обе собаки одинакового возраста";
-        }
-    }
-}
-
-class Dog extends Pet {
-    constructor(name, age, breed) {
-        super(name, age);
-        this.breed = breed;
-    }
-
-    describe() {
-        return `Имя: ${this.name}, возраст: ${this.age}, порода: ${this.breed}`;
-    }
-
-    bark() {
-        console.log(`${this.name} лает`);
-    }
-
-    fetch(item) {
-        console.log(`${this.name} приносит ${item}`);
-    }
-}
-
-class GuardDog extends Dog {
-    constructor(name, age, breed, trainingLevel) {
-        super(name, age, breed);
-        this.trainingLevel = trainingLevel;
-    }
-
-    guard() {
-        console.log(`${this.name} охраняет территорию на уровне ${this.trainingLevel}`);
-    }
-
-    bark() {
-        if (this.trainingLevel > 5) {
-            console.log(`${this.name} лает громко!`);
-        } else {
-            console.log(`${this.name} лает нормально`);
-        }
-    }
-}
-
-
-let dog = new Dog("Doggy", 2, "shpitz");
-console.log(dog.describe()); 
-dog.bark(); 
-dog.fetch("палка"); 
-console.log(dog.isOld()); 
-
-let pet1 = new Pet("Pitty", 3);
-let pet2 = new Pet("Biggy", 12);
-console.log(Pet.compareAges(pet1, pet2)); 
-
-let guardDog = new GuardDog("Mixxy", 6, "Shepherd", 7);
-console.log(guardDog.describe()); 
-guardDog.guard(); 
-guardDog.bark(); 
-
+  
+  Dog.prototype = Object.create(Pet.prototype);
+  Dog.prototype.constructor = Dog;
+  
+  
+  function GuardDog(name, age, breed, trainingLevel) {
+    Dog.call(this, name, age, breed); 
+    this.trainingLevel = trainingLevel;
+  
+    this.guard = function () {
+      console.log(`${this.name} охраняет территорию на уровне ${this.trainingLevel}`);
+    };
+  
+    this.bark = function () {
+      if (this.trainingLevel > 5) {
+        console.log(`${this.name} лает громко!`);
+      } else {
+        console.log(`${this.name} лает нормально`);
+      }
+    };
+  }
+  
+ 
+  GuardDog.prototype = Object.create(Dog.prototype);
+  GuardDog.prototype.constructor = GuardDog;
+  
+  
+  let dog = new Dog("Doggy", 2, "shpitz");
+  console.log(dog.describe()); 
+  dog.bark(); 
+  dog.fetch("палка"); 
+  console.log(dog.isOld()); 
+  
+  let pet1 = new Pet("Pitty", 3);
+  let pet2 = new Pet("Biggy", 12);
+  console.log(Pet.compareAges(pet1, pet2));
+  
+  let guardDog = new GuardDog("Mixxy", 6, "Shepherd", 7);
+  console.log(guardDog.describe()); 
+  guardDog.guard(); 
+  guardDog.bark(); 
